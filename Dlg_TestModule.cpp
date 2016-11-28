bdd190c2ba5f9549b3a5cf9d029432954c526c91
greensky01 <wl370408@126.com>
2016-11-28 10:21:23 +0800

    feature - parse config JSON
    parse and generate label vector

--- Dlg_TestModule.cpp
+++ Dlg_TestModule.cpp
@@ -3,6 +3,9 @@
 #include <QtGui/QMessageBox>
 #include <iostream>
 #include "ClassifierEMG.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <QtGui/QMessageBox>
 
 Dlg_TestModule::Dlg_TestModule( QWidget* parent /*= NULL*/ )
 	: QDialog(parent)
@@ -47,6 +50,20 @@ void Dlg_TestModule::on_BtnImportData_clicked()
 
 void Dlg_TestModule::on_BtnCreateClassifier_clicked()
 {
+	// read config file, and generate label vector
+	_parseTrainConfig();
+	std::vector<int> labelVec;
+	for (size_t i=0; i<_commandVec.size(); i++)
+	{
+		size_t tsp = _commandVec[i]->TotalSample;
+		int label = _commandVec[i]->Command;
+		for (size_t j=0; j<tsp; j++)
+		{
+			labelVec.push_back(label);
+		}
+	}
+
+	// read raw data
 	std::vector<std::vector<double> > trainData;
 	std::ifstream raw_data(_mTrainDataPath);
 
@@ -56,11 +73,57 @@ void Dlg_TestModule::on_BtnCreateClassifier_clicked()
 	{
 		for (int i=0; i<8; i++)
 		{
-			//std::getline(raw_data,val/*,','*/);
 			raw_data >> val;
 			data_tmp[i] = std::atof(val.c_str());
 		}
 		trainData.push_back(data_tmp);
 	}
 
+}
+
+void Dlg_TestModule::_parseTrainConfig()
+{
+	using namespace boost::property_tree;
+	ptree root;
+
+	try
+	{
+		// load the json file in this ptree
+		read_json(_mTrainConfigPath,root);
+	}
+	catch (json_parser::json_parser_error& e)
+	{
+		std::cout << "Error: Invalid JSON file!" << std::endl;
+		std::cout << "[Info] " << e.what() << std::endl;
+		system("pause");
+		return;
+	}
+
+	std::string _type = root.get<std::string>("type","");
+	if(_type != "Training Series")
+	{
+		QMessageBox::warning(NULL, "warning", "Invalid configuration file.", QMessageBox::Ok);
+		return;
+	}
+
+	ptree Actions = root.get_child("actions");
+	std::vector< std::shared_ptr<_commandInfo> > InfoVec;
+	for (ptree::iterator it=Actions.begin(); it!=Actions.end(); it++)
+	{
+		std::string name = it->second.get<std::string>("name");
+		int command = it->second.get<int>("command");
+		size_t dura = it->second.get<size_t>("duration");
+		size_t smRt = it->second.get<size_t>("sample_rate");
+		size_t tSp = it->second.get<size_t>("total_sample");
+		std::shared_ptr<_commandInfo> cinfo(new _commandInfo());
+		cinfo->Name = name;
+		cinfo->Command = command;
+		cinfo->Duration = dura;
+		cinfo->SampleRate = smRt;
+		cinfo->TotalSample = tSp;
+		InfoVec.push_back(cinfo);
+	}
+
+	_commandVec.clear();
+	_commandVec = InfoVec;
 }
